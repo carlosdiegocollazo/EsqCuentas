223a0b0142604b29312f1c838592201d18f939c1
Carlos Diego Collazo <54947450+carlosdiegocollazo@users.noreply.github.com>
2020-10-10 17:18:28 -0300

    primeros bocetos de diagramas

--- app/sharedbkp/menu.component.js
+++
@@ -1,8 +0,0 @@
-Vue.component('menu-item', function (resolve, reject){
-	axios.get('./app/shared/menuView.html').then( function (view){
-		resolve({
-			//props:['title','menuItems'],
-			template: view.data
-		})
-	})
-})
