1b9f5b34e67b22d16ae2cd6b2bbc143886d6a7ce
Carlos Diego Collazo <54947450+carlosdiegocollazo@users.noreply.github.com>
2020-10-12 17:50:47 -0300

    login funcionando

--- api/services/usuarios.js
+++ api/services/usuarios.js
@@ -1,24 +1,25 @@
 let Usuario = {
-	
-	getRecordById: async function (tabla, idTabla, id){
-		let sql 		= `
+
+    getRecordById: async function(tabla, idTabla, id) {
+        let sql = `
 							SELECT *
 							FROM ${tabla}
 							WHERE
 							${idTabla} = ${id}
 						`
-		let response 	= {error: "No se encontraron registros"}
-		let res 		= await conn.query(sql);
-		if (res.code) {
-	 		response 	= {error: "Error en consulta SQL"};
-	 	}else if (res.length>0) {
-			response 	= res[0];
-			}
-		return response; 
-	},
+        let response = { error: "No se encontraron registros" }
+        let res = await conn.query(sql);
+        if (res.code) {
+            response = { error: "Error en consulta SQL" };
+        } else if (res.length > 0) {
+            response = res[0];
+        }
+        return response;
+    },
 
-	usuarioLogin: async function(usuario, password){
-		let sql =  `
+    usuarioLogin: async function(usuario, password) {
+        
+        let sql = `
 				SELECT * 
 				FROM usuarios 
 				WHERE usuarios.email = '${usuario}'
@@ -27,88 +28,88 @@ let Usuario = {
 				&&
 				usuarios.activo = 1
 			`
-		let response = {error: "Usuario / Contraseña incorrectos"}
-		let usuarios = await conn.query(sql);
-		try {
-			if (usuarios.length>0) {
-				let usuario 	= usuarios[0];
-				const payload 	= {usuario:  usuario};
-				const token 	= jwt.sign(payload, conn.llave, {expiresIn: 18000});
-				response 		= {response: usuario,token: token};
-			}
-		} catch(e) {
-			console.log(e);
-			}
-		return response;
-	},
+        let response = { error: "Usuario / Contraseña incorrectos - " }
+        let usuarios = await conn.query(sql);
+        try {
+            if (usuarios.length > 0) {
+                let usuario = usuarios[0];
+                const payload = { usuario: usuario };
+                const token = jwt.sign(payload, conn.llave, { expiresIn: 18000 });
+                response = { response: usuario, token: token };
+            }
+        } catch (e) {
+            console.log(e);
+        }
+        return response;
+    },
 
-	obtenerUsuarios: async function(){
-		let sql 		= `
+    obtenerUsuarios: async function() {
+        let sql = `
 							SELECT * FROM usuarios
 							WHERE usuarios.activo = 1
 						`
-		let response 	= {error: "No se encontraron usuarios"}
-		let resultado 	= await conn.query(sql);
-		if (resultado.code) {
-	 		response 	= {error: "Error en consulta SQL"};
-	 	}else if (resultado.length>0) {
-			response 	= {response: resultado}
-			}
-		return response;
-	},
+        let response = { error: "No se encontraron usuarios" }
+        let resultado = await conn.query(sql);
+        if (resultado.code) {
+            response = { error: "Error en consulta SQL" };
+        } else if (resultado.length > 0) {
+            response = { response: resultado }
+        }
+        return response;
+    },
 
-	obtenerUsuariosall: async function(){
-		let sql 		= `
+    obtenerUsuariosall: async function() {
+        let sql = `
 							SELECT * FROM usuarios
 						`
-		let response 	= {error: "No se encontraron usuarios"}
-		let resultado 	= await conn.query(sql);
-		if (resultado.code) {
-	 		response 	= {error: "Error en consulta SQL"};
-	 	}else if (resultado.length>0) {
-			response 	= {response: resultado}
-			}
-		return response;
-	},
+        let response = { error: "No se encontraron usuarios" }
+        let resultado = await conn.query(sql);
+        if (resultado.code) {
+            response = { error: "Error en consulta SQL" };
+        } else if (resultado.length > 0) {
+            response = { response: resultado }
+        }
+        return response;
+    },
 
-	obtenerUsuarioPorId: async function (id){
-	 let sql = `
+    obtenerUsuarioPorId: async function(id) {
+        let sql = `
 	  			SELECT * FROM usuarios
 	  			WHERE 
-	  			usuarios.idper = '${id}' 
+	  			usuarios.idusu = '${id}' 
 	  			&& usuarios.activo = 1
 	 		`
-	 let usuarios 	= []
-	 let response 	= {error: `No existe el usuario con ID: ${id}`}
-	 usuarios 		= await conn.query(sql)
-	 if (usuarios.code) {
-	 	response 	= {error: "Error en consulta SQL"};
-	 }else if (usuarios.length > 0) {
-	 	response 	= {response: usuarios[0]}
-	 }
-	 return response;
-	},
+        let usuarios = []
+        let response = { error: `No existe el usuario con ID: ${id}` }
+        usuarios = await conn.query(sql)
+        if (usuarios.code) {
+            response = { error: "Error en consulta SQL" };
+        } else if (usuarios.length > 0) {
+            response = { response: usuarios[0] }
+        }
+        return response;
+    },
 
-	obtenerUsuarioPorEmail: async function (mail){
-	 let sql = `
-	  			SELECT usuarios.idper 
+    obtenerUsuarioPorEmail: async function(mail) {
+        let sql = `
+	  			SELECT usuarios.idusu 
 	  			FROM usuarios
 	  			WHERE 
 	  			usuarios.email = '${mail}'
 	 		`
-	 let usuarios 	= []
-	 let response 	= {error: `No existe el usuario con mail : ${mail}`}
-	 usuarios 		= await conn.query(sql)
-	 if (usuarios.code) {
-	 	response 	= {error: "Error en consulta SQL"};
-	 }else if (usuarios.length > 0) {
-	 	response 	= {response: usuarios[0]}
-	 }
-	 return response;
-	},
+        let usuarios = []
+        let response = { error: `No existe el usuario con mail : ${mail}` }
+        usuarios = await conn.query(sql)
+        if (usuarios.code) {
+            response = { error: "Error en consulta SQL" };
+        } else if (usuarios.length > 0) {
+            response = { response: usuarios[0] }
+        }
+        return response;
+    },
 
-	ingresarUsuario: async function(usuario){
-		let sql = `
+    ingresarUsuario: async function(usuario) {
+        let sql = `
 					INSERT INTO usuarios
 					  		(
 							email,
@@ -140,28 +141,28 @@ let Usuario = {
 							'${usuario.activo}'
 							)
 				`
-		let response 		= {error: "No se pudo crear el usuario"}
-		let mail 			= usuario.email;
-		let existeUsuario 	= await this.obtenerUsuarioPorEmail(mail);
-		if (existeUsuario.error) {
-			try {
-				let resultado 	= await conn.query(sql);
-				if (resultado.code) {
-		 			response 	= {error: "Error en consulta SQL"};
-		 		}else if (resultado.insertId) {
-					response 	= {response: "Usuario creado correctamente"}
-				}
-			} catch(error) {
-				console.log(error);
-				}
-		}else {
-			response 			= {error: `Ya existe usuario con email : ${mail}`}
-			}
-		return response;
-	},
+        let response = { error: "No se pudo crear el usuario" }
+        let mail = usuario.email;
+        let existeUsuario = await this.obtenerUsuarioPorEmail(mail);
+        if (existeUsuario.error) {
+            try {
+                let resultado = await conn.query(sql);
+                if (resultado.code) {
+                    response = { error: "Error en consulta SQL" };
+                } else if (resultado.insertId) {
+                    response = { response: "Usuario creado correctamente" }
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+            response = { error: `Ya existe usuario con email : ${mail}` }
+        }
+        return response;
+    },
 
-	actualizarUsuario: async function(usuario, id){
-		let sql 		= `
+    actualizarUsuario: async function(usuario, id) {
+        let sql = `
 							UPDATE usuarios 
 							SET 
 								email 			= '${usuario.email}',
@@ -177,45 +178,45 @@ let Usuario = {
 								observaciones 	= '${usuario.observaciones}',
 								activo 			= '${usuario.activo}'
 							WHERE
-							usuarios.idper = '${id}'
+							usuarios.idusu = '${id}'
 						`
-		let response 		= {error: "No se pudo actualizar usuario"}
-		let existeUsuario 	= await this.obtenerUsuarioPorId(id);
-		if (!existeUsuario.error) {
-			let resultado 	= await conn.query(sql);
-			if (resultado.code) {
-	 			response 	= {error: "Error en consulta SQL"};
-	 		}else if (resultado.affectedRows>0) {
-				response 	= {response: "Usuario actualizado correctamente"}
-			}
-		}else{
-			response 		= {error: `No existe usuario con Id: ${id}`}
-			}
-		return response;
-	},	
+        let response = { error: "No se pudo actualizar usuario" }
+        let existeUsuario = await this.obtenerUsuarioPorId(id);
+        if (!existeUsuario.error) {
+            let resultado = await conn.query(sql);
+            if (resultado.code) {
+                response = { error: "Error en consulta SQL" };
+            } else if (resultado.affectedRows > 0) {
+                response = { response: "Usuario actualizado correctamente" }
+            }
+        } else {
+            response = { error: `No existe usuario con Id: ${id}` }
+        }
+        return response;
+    },
 
-	eliminarUsuario: async function(id){
-		let sql 		= `
+    eliminarUsuario: async function(id) {
+        let sql = `
 							UPDATE usuarios 
 							SET 
 							activo = 0 
 							WHERE
-							usuarios.idper = '${id}'
+							usuarios.idusu = '${id}'
 						`
-		let response 			= {error: "No se pudo eliminar usuario"}
-		let existeUsuario 		= await this.obtenerUsuarioPorId(id);
-		if (!existeUsuario.error) {
-			let resultado 		= await conn.query(sql);
-			if (resultado.code) {
-	 			response 		= {error: "Error en consulta SQL"};
-	 		}else if (resultado.affectedRows>0) {
-					response 	= {response: "Usuario eliminado correctamente"}
-				}
-		}else {
-			response 			= {error: `No existe usuario con Id: ${id}`}
-			}
-		return response;
-	},
+        let response = { error: "No se pudo eliminar usuario" }
+        let existeUsuario = await this.obtenerUsuarioPorId(id);
+        if (!existeUsuario.error) {
+            let resultado = await conn.query(sql);
+            if (resultado.code) {
+                response = { error: "Error en consulta SQL" };
+            } else if (resultado.affectedRows > 0) {
+                response = { response: "Usuario eliminado correctamente" }
+            }
+        } else {
+            response = { error: `No existe usuario con Id: ${id}` }
+        }
+        return response;
+    },
 
 }
 
